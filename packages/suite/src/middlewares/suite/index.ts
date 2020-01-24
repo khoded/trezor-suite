7c207c108ee0cc3b7fdd599092b1242d71923ab5
martin <mvarmuza@gmail.com>
2020-01-24 12:14:59 +0100

    move reducer

--- packages/suite/src/middlewares/suite/index.ts
+++ packages/suite/src/middlewares/suite/index.ts
@@ -1,6 +1,5 @@
 import log from './logMiddleware';
 import suite from './suiteMiddleware';
 import redirect from './redirectMiddleware';
-import firmware from '../firmware/firmwareMiddleware';
 
-export default [log, redirect, suite, firmware];
+export default [log, redirect, suite];
